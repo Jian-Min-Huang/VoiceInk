c010e3dbdab28189bc84fed6fc6c1e96ae62eb7d
Vincent Huang <vincent.huang@opennet.tw>
2026-02-24 20:10:34 +0800

    feat(whisper): prepend voice output annotation emoji to pasted text

--- VoiceInk/Whisper/WhisperState.swift
+++ VoiceInk/Whisper/WhisperState.swift
@@ -450,7 +450,7 @@ class WhisperState: NSObject, ObservableObject {
             }
 
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
-                CursorPaster.pasteAtCursor(textToPaste + " ")
+                CursorPaster.pasteAtCursor("🗣️ " + textToPaste)
 
                 let powerMode = PowerModeManager.shared
                 if let activeConfig = powerMode.currentActiveConfiguration, activeConfig.isAutoSendEnabled {
